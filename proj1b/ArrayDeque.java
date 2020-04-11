a331b470b126b9183a2b0b702df11aee2f944576
htlee <w29593617@gmail.com>
2020-04-11 11:10:34 +0800

    '2nd'

--- proj1b/ArrayDeque.java
+++ proj1b/ArrayDeque.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+public class ArrayDeque<T> implements Deque<T> {
+    @Override
+    public void addFirst(T item) {
 
-public class ArrayDeque {
+    }
+
+    @Override
+    public void addLast(T item) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void printDeque() {
+
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
 }
