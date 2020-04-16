a75361c67de45b237fd168c529af1874b5fbb931
htlee <w29593617@gmail.com>
2020-04-16 19:15:03 +0800

    19:15

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -46,10 +46,6 @@ public class GuitarString {
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        try {
-            buffer.peek();
-        } catch (Exception e) {
-        }
         return buffer.peek();
     }
 }
